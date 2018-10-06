1cfc53e9deefc8b520ced948d647e3258891ab10
Sevinj Azizli <azizli.sevinc@gmail.com>
2018-10-06 19:21:33 +0200

    Create custom methods and queries

--- src/main/java/com/springbootjparest/DAO/AlienRepo.java
+++ src/main/java/com/springbootjparest/DAO/AlienRepo.java
@@ -1,7 +1,16 @@
 package com.springbootjparest.DAO;
 
 import com.springbootjparest.model.Alien;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AlienRepo extends CrudRepository<Alien,Integer> {
+
+    List<Alien> findByZlang(String zlang);
+    List<Alien> findByIdGreaterThan(int id);
+
+    @Query(value = "from Alien where zlang=?1 order by name ")
+    List<Alien> findByZlangSortedByName(String zlang);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-insert into alien values (1,'Java','Seva');
+insert into alien values (1,'Seva','Java');
+insert into alien values (2,'Tom','Java');
+insert into alien values (3,'John','PHP');
+insert into alien values (4,'Anna','C#');
