5cd5e972ccc47083902132a412d885740f32a252
Sevinj Azizli <azizli.sevinc@gmail.com>
2018-10-07 14:57:06 +0200

    CRUD operations with REST methods

--- src/main/java/com/springbootjparest/controller/AlienController.java
+++ src/main/java/com/springbootjparest/controller/AlienController.java
@@ -5,39 +5,65 @@ import com.springbootjparest.model.Alien;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 public class AlienController {
 
     @Autowired
     AlienRepo alienRepo;
 
-    @RequestMapping("/aliens")
-    @ResponseBody
+    @GetMapping("/aliens")
     public List<Alien> getAliens (){
 
         return alienRepo.findAll();
     }
 
-
-    @RequestMapping("/aliens/{alienId}")
-    @ResponseBody
+    @GetMapping("/aliens/{alienId}")
     public Optional<Alien> getAliens (@PathVariable("alienId") int id){
 
         return alienRepo.findById( id );
 
+    }
+
+// send request in raw body:
+//    @PostMapping(path = "/alien")
+//    public Alien addAlien (@RequestBody Alien alien){
+//
+//       alienRepo.save( alien );
+//        return alien;
+//    }
+
+
+    // send request in multipart/form-data:
+    @PostMapping(path = "/alien")
+    public Alien addAlien ( Alien alien) {
+
+        alienRepo.save( alien );
+        return alien;
 
     }
 
+    // send request in raw body:
+    @DeleteMapping (path = "/alien/{id}")
+    public String deleteAlien (@PathVariable int id){
 
+        Alien a = alienRepo.getOne(id );
+       alienRepo.delete( a );
+        return "deleted!";
+    }
 
+    // send request in raw body:
+    @PutMapping (path = "/alien")
+    public Alien updateorcreateAlien (@RequestBody Alien alien){
+
+        alienRepo.save( alien );
+
+        return alien;
+    }
 
 }
